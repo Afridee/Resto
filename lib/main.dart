28d01cc3e15b8bee04bc4df4369caa2c7f70a7b0
Ahmed Afridee <a.afridee47@gmail.com>
2020-05-23 05:19:33 +0600

    Final Product

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:project_resto/Screens/Records.dart';
 import 'package:flutter/material.dart';
 import 'package:project_resto/Screens/login_screen.dart';
 import 'package:project_resto/Screens/resetPassword.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() => runApp(
   MaterialApp(
